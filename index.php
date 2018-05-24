9ea462677129932358b2e87313a5cb4f996c5719
marcolla.hc <marcolla.hc@gmail.com>
2018-05-23 23:04:29 -0300

    Template do admin

--- index.php
+++ index.php
@@ -4,13 +4,21 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \MarcollaHC\Pages;
+use \MarcollaHC\PagesAdmin;
 
 $app = new Slim();
 $app->config("debug", true);
+
 $app->get("/", function() {
 	$page = new Pages();
 	$page->setTemplate("index");
 });
+
+$app->get("/admin", function() {
+	$page = new PagesAdmin();
+	$page->setTemplate("index");
+});
+
 $app->run();
 
- ?>
+?>
