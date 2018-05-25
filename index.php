06dc6133d37b8047d9cbe5cc3e0b8431a4958648
marcolla.hc <marcolla.hc@gmail.com>
2018-05-25 00:34:11 -0300

    Login do administrador

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php 
 
+session_start();
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \MarcollaHC\Pages;
 use \MarcollaHC\PagesAdmin;
+use \MarcollaHC\Model\Users;
 
 $app = new Slim();
 $app->config("debug", true);
@@ -15,8 +17,29 @@ $app->get("/", function() {
 });
 
 $app->get("/admin", function() {
+	Users::verify();
 	$page = new PagesAdmin();
 	$page->setTemplate("index");
+});
+
+$app->get("/admin/login", function () {
+	$page = new PagesAdmin([
+		"header" => false,
+		"footer" => false
+	]);
+	$page->setTemplate("login");
+});
+
+$app->post("/admin/login", function () {
+	Users::login($_POST["login"], $_POST["password"]);
+	header("location: http://localhost/Exercises/Udemy/curso-completo-de-php-7/secao-24/projeto/admin");
+	exit;
+});
+
+$app->get("/admin/logout", function () {
+	Users::logout();
+	header("location: http://localhost/Exercises/Udemy/curso-completo-de-php-7/secao-24/projeto/admin");
+	exit;
 });
 
 $app->run();
